4ea418425ad18d74f4f1eda24b90e7676e952714
Miguel Angel Piña Avelino <miguel_pinia@ciencias.unam.mx>
2020-04-15 09:18:33 -0500

    Añade el número de iteraciones para los experimentos.

--- src/main/java/org/mx/unam/imate/concurrent/algorithms/utils/Parameters.java
+++ src/main/java/org/mx/unam/imate/concurrent/algorithms/utils/Parameters.java
@@ -17,6 +17,7 @@ public class Parameters {
     private int numThreads;
     private AlgorithmsType algType;
     private int structSize;
+    private int numIterExps;
 
     public Parameters() {
     }
@@ -49,6 +50,31 @@ public class Parameters {
         this.structSize = structSize;
     }
 
+    /**
+     * Los parámetros necesarios para construir la ejecución de un árbol
+     * generador distribuido son el tipo de gráfica <i>type</i>, el tipo de
+     * algoritmo que se va a ejecutar <i>algType</i>, el número de vértices (en
+     * este caso, se debe tomar en cuenta que si se genera un toro, debe darse
+     * el shape de la malla que genera el toro o el número de vértices de la
+     * gráfica aleatoria) <i>shape</i>, el número de hilos que van a ejecutar el
+     * algoritmo <i>numThreads</i>, el tamaño de la estructura donde van a
+     * almacenarse las tareas durante la ejecución del algoritmo
+     * <i>structSize</i> y si se va a incluir un objeto que guarde información
+     * para realizar un reporte de la ejecución.
+     *
+     * @param type El tipo de gráfica.
+     * @param algType El tipo de algoritmo.
+     * @param shape El shape de la gráfica.
+     * @param numThreads El número de hilos.
+     * @param structSize El tamaño de la estructura.
+     * @param report Si se va a realizar un reporte.
+     * @param numIterExps El número de iteraciones para un experimento.
+     */
+    public Parameters(GraphType type, AlgorithmsType algType, int shape, int numThreads, int structSize, boolean report, int numIterExps) {
+        this(type, algType, shape, numThreads, structSize, report);
+        this.numIterExps = numIterExps;
+    }
+
     public GraphType getType() {
         return type;
     }
@@ -75,6 +101,14 @@ public class Parameters {
 
     public boolean isSpecialExecution() {
         return (algType == AlgorithmsType.OURS_V1 || algType == AlgorithmsType.OURS_V2);
+    }
+
+    public int getNumIterExps() {
+        return numIterExps;
+    }
+
+    public void setNumIterExps(int numIterExps) {
+        this.numIterExps = numIterExps;
     }
 
     public void setType(GraphType type) {
