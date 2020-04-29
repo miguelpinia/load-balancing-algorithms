57272d59cb6a75cabf48336986ac8734c5a4a27d
Miguel Angel Piña Avelino <miguel_pinia@ciencias.unam.mx>
2020-04-29 14:09:18 -0500

    Agrega Main para el archivo .jar.
    Incluye un archivo de propiedades para ejecutarse con el generado y
    otro con ejemplos de ejecuciones de experimentos.

---
+++ src/main/java/org/mx/unam/imate/concurrent/main/Main.java
@@ -0,0 +1,50 @@
+package org.mx.unam.imate.concurrent.main;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.mx.unam.imate.concurrent.algorithms.experiments.TestBattery;
+import org.mx.unam.imate.concurrent.algorithms.experiments.spanningTree.StepSpanningTreeType;
+import org.mx.unam.imate.concurrent.datastructures.GraphType;
+
+public class Main {
+
+    public static void main(String[] args) {
+        File f = new File("properties.props");
+        Map<String, String> props = readProperties(f);
+        System.out.println(props);
+        TestBattery battery = new TestBattery((GraphType.valueOf(props.get("graphType"))),
+                Integer.valueOf(props.get("vertexSize")),
+                StepSpanningTreeType.valueOf(props.get("stepSpanningType")),
+                Integer.valueOf(props.get("iterations")));
+        battery.compareAlgs();
+    }
+
+    private static Map<String, String> readProperties(File f) {
+        Map<String, String> props = new HashMap<>();
+        BufferedReader br;
+        try {
+            br = new BufferedReader(new FileReader(f));
+            String line;
+            while ((line = br.readLine()) != null) {
+                String data[] = line.split(":");
+                props.put(data[0].trim(), data[1].trim());
+            }
+        } catch (FileNotFoundException ex) {
+            System.out.println("Archivo no encontrado, cerrando el programa");
+            System.exit(0);
+        } catch (IOException ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return props;
+
+    }
+
+}
