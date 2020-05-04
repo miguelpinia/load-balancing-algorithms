5ceb7dab06661bd9e667b5dd962b50c26950b7fb
Miguel Angel Piña Avelino <miguel_pinia@ciencias.unam.mx>
2020-05-04 12:51:29 -0500

    Agrega script para detener los servicios de MI COMPUTADORA local.

---
+++ stopServices.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+sudo service postgresql stop
+sudo service mysql stop
+sudo service gdm3 stop
+sudo service docker stop
+sudo service snapd stop
+sudo service cups stop
+sudo service bluetooth stop
+sudo service networking stop
+sudo service network-manager stop
+sudo service systemd-journald stop
