134055d842699f094e127bc19ea8a8ddeebf1a99
Miguel Angel Piña Avelino <miguel_pinia@ciencias.unam.mx>
2020-08-01 18:20:56 -0500

    Adds first version of the parser.

---
+++ analyzer/main.py
@@ -0,0 +1,59 @@
+from pprint import pprint
+import re
+
+
+def num_processors(data):
+    regex = r"(Processors:) (\d+)"
+    match = re.search(regex, data)
+    if match is not None:
+        return match.group(2)
+    return None
+
+
+def chunks(lst, processors):
+    for i in range(0, len(lst), processors):
+        yield lst[i: i + processors]
+
+
+def get_info_execution(info):
+    results = info[1:-1].split(',')
+    results = [val.strip().split('=') for val in results]
+    results = {key: value for [key, value] in results}
+    results['algorithms'] = results['algorithms'].split(';')
+    results['directed'] = bool(results['directed'])
+    results['iterations'] = int(results['iterations'])
+    results['vertexSize'] = int(results['vertexSize'])
+    return results
+
+
+def parse_file(path_file):
+    with open(path_file, 'r') as log:
+        data = log.read()
+        processors = int(num_processors(data))
+        info = data.split('\n')
+        idx_threads = [
+            info.index('Threads: {}'.format(thread))
+            for thread in range(1, int(processors) + 1)]
+        results = get_info_execution(info[0])
+        execs = {}
+        for thread in range(1, processors + 1):
+            dict_thread = {}
+            pos = idx_threads[thread - 1]
+            algs = {alg: info.index('Algorithm:\t{}'.format(alg), pos, pos + 312)
+                    for alg in results['algorithms']}
+            for key in algs:
+                datos = list(filter(lambda x: x != '',
+                                    info[algs[key] + 2:algs[key] + 26]))
+                datos = [datos[i:i + 4] for i in range(0, len(datos), 4)]
+                datos = {idx: {output[0]: int(output[1].strip())
+                               for val in line if (output := val.split(':'))
+                               is not None}
+                         for idx, line in enumerate(datos)}
+                dict_thread[key] = datos
+            execs['thread-{}'.format(thread)] = dict_thread
+        results['executions'] = execs
+        pprint(results)
+        return results
+
+
+parse_file('../output.log')
