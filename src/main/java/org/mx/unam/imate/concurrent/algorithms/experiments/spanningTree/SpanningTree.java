781e908c95c0fdd991e9c8a475a6b835e4fc8c11
Miguel Angel Piña Avelino <miguel_pinia@ciencias.unam.mx>
2020-08-31 17:15:24 -0500

    Separa lógica de estadísticas del de spanning tree.

--- src/main/java/org/mx/unam/imate/concurrent/algorithms/experiments/spanningTree/SpanningTree.java
+++ src/main/java/org/mx/unam/imate/concurrent/algorithms/experiments/spanningTree/SpanningTree.java
@@ -1,20 +1,15 @@
 package org.mx.unam.imate.concurrent.algorithms.experiments.spanningTree;
 
-import org.mx.unam.imate.concurrent.algorithms.experiments.spanningTree.stepSpanningTree.StepSpanningTreeLookUp;
-import org.mx.unam.imate.concurrent.algorithms.experiments.spanningTree.stepSpanningTree.AbstractStepSpanningTree;
-
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicIntegerArray;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import org.mx.unam.imate.concurrent.algorithms.WorkStealingStruct;
+import org.mx.unam.imate.concurrent.algorithms.experiments.spanningTree.stepSpanningTree.AbstractStepSpanningTree;
+import org.mx.unam.imate.concurrent.algorithms.experiments.spanningTree.stepSpanningTree.StepSpanningTreeLookUp;
 import org.mx.unam.imate.concurrent.algorithms.utils.Parameters;
 import org.mx.unam.imate.concurrent.algorithms.utils.Report;
-import org.mx.unam.imate.concurrent.algorithms.utils.Result;
 import org.mx.unam.imate.concurrent.datastructures.graph.Graph;
 import org.mx.unam.imate.concurrent.datastructures.graph.GraphUtils;
 
@@ -24,28 +19,7 @@ import org.mx.unam.imate.concurrent.datastructures.graph.GraphUtils;
  */
 public class SpanningTree {
 
-    private final Parameters params;
-
-    public SpanningTree(Parameters parameters) {
-        params = parameters;
-    }
-
-    public List<Report> experiment(Graph graph) {
-        List<Report> reports = new ArrayList<>();
-        final int[] roots = GraphUtils.stubSpanning(graph, params.getNumThreads());
-        for (int i = 0; i < params.getNumIterExps(); i++) {
-//            System.out.println("Iteración " + i + ", Algoritmo: " + params.getAlgType());
-            Report report = new Report();
-            report.setAlgType(params.getAlgType());
-            report.setGraphType(params.getType());
-            Graph tree = spanningTree(graph, roots, report);
-            assert (GraphUtils.isTree(tree));
-            reports.add(report);
-        }
-        return reports;
-    }
-
-    public Graph spanningTree(Graph graph, int[] roots, Report report) {
+    public Graph spanningTree(Graph graph, int[] roots, Report report, Parameters params) {
         Thread[] threads = new Thread[params.getNumThreads()];
         AtomicIntegerArray colors = new AtomicIntegerArray(graph.getNumberVertices());
         AtomicIntegerArray parents = new AtomicIntegerArray(GraphUtils.initializeParents(graph.getNumberVertices()));
@@ -91,70 +65,6 @@ public class SpanningTree {
         }
         Graph tree = GraphUtils.buildFromParents(parents, graph.getRoot(), graph.isDirected());
         return tree;
-    }
-
-    public Result statistics(List<Report> reports) {
-        Collections.sort(reports);
-
-        System.out.println(String.format("%n%nGraph:\t%s%nAlgorithm:\t%s%n",
-                reports.get(0).getGraphType(),
-                reports.get(0).getAlgType()));
-        reports.forEach((r) -> {
-            System.out.println(String.format("Execution time: %d%nTakes: %d%nPuts: %d%nSteals: %d%n",
-                    r.getExecutionTime(), r.getTakes(), r.getPuts(), r.getSteals()));
-        });
-        long best = reports.get(0).getExecutionTime();
-        reports = removeWorstAndBest(reports);
-
-        List<Long> values2Median = new ArrayList<>();
-        List<Integer> takes = new ArrayList<>();
-        List<Integer> puts = new ArrayList<>();
-        List<Integer> steals = new ArrayList<>();
-
-        reports.forEach((reporte) -> {
-            values2Median.add(reporte.getExecutionTime());
-            takes.add(reporte.getTakes());
-            puts.add(reporte.getPuts());
-            steals.add(reporte.getSteals());
-        });
-
-        long median = median(values2Median);
-        double average = values2Median.stream().mapToDouble(a -> a).average().getAsDouble();
-        double averageTakes = takes.stream().mapToDouble(a -> a).average().getAsDouble();
-        double averagePuts = puts.stream().mapToDouble(a -> a).average().getAsDouble();
-        double averageSteals = steals.stream().mapToDouble(a -> a).average().getAsDouble();
-
-//        System.out.println("Gráfica:\t" + reports.get(0).getGraphType());
-//        System.out.println("Algoritmo:\t" + reports.get(0).getAlgType());
-        System.out.println("Best time:\t" + best + " ns");
-        System.out.println("Best time:\t" + best / 1000000 + " ms");
-        System.out.println("Median time:\t" + median + " ns");
-        System.out.println("Median time:\t" + median / 1000000 + " ms");
-        System.out.println("Time Average:\t" + average + " ns");
-        System.out.println("Time Average:\t" + average / 1000000 + " ms");
-        System.out.println("Takes Average:\t" + averageTakes);
-        System.out.println("Puts Average:\t" + averagePuts);
-        System.out.println("Steals Average:\t" + averageSteals);
-
-        return new Result(reports.get(0).getGraphType(),
-                reports.get(0).getAlgType(), median, average,
-                averageTakes, averagePuts, averageSteals, best);
-    }
-
-    private List<Report> removeWorstAndBest(List<Report> reports) {
-
-        reports.remove(0);
-        reports.remove(reports.size() - 1);
-        return reports;
-    }
-
-    private long median(List<Long> values) {
-        Collections.sort(values);
-        int n = values.size();
-        if (n % 2 != 0) {
-            return values.get(n / 2);
-        }
-        return (values.get((n - 1) / 2) + values.get(n / 2)) / 2;
     }
 
 }
