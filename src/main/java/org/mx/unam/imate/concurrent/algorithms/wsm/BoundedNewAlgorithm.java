03642cf74f642d6ba9f12dad35a4b2772689875f
Miguel Angel Piña Avelino <miguel_pinia@ciencias.unam.mx>
2020-09-01 16:15:37 -0500

    Delete unnecessary header comments.

--- src/main/java/org/mx/unam/imate/concurrent/algorithms/wsm/BoundedNewAlgorithm.java
+++ src/main/java/org/mx/unam/imate/concurrent/algorithms/wsm/BoundedNewAlgorithm.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package org.mx.unam.imate.concurrent.algorithms.wsm;
 
 import java.util.concurrent.atomic.AtomicBoolean;
