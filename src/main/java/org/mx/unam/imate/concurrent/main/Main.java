3ba1826c9b0ad153cedb20273d9f5d6bc9b17665
Miguel Angel Piña Avelino <miguel_pinia@ciencias.unam.mx>
2020-09-01 20:35:23 -0500

    Elimina método no utilizado.

--- src/main/java/org/mx/unam/imate/concurrent/main/Main.java
+++ src/main/java/org/mx/unam/imate/concurrent/main/Main.java
@@ -1,10 +1,7 @@
 package org.mx.unam.imate.concurrent.main;
 
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
@@ -42,26 +39,6 @@ public class Main {
         boolean stealTime = (Boolean) props.get("stealTime");
         TestBattery battery = new TestBattery(type, vertexSize, stepType, iterations, types, directed, stealTime);
         battery.compareAlgs();
-    }
-
-    private static Map<String, String> readProperties(File f) {
-        Map<String, String> props = new HashMap<>();
-        BufferedReader br;
-        try {
-            br = Files.newBufferedReader(f.toPath(), StandardCharsets.UTF_8);
-            String line;
-            while ((line = br.readLine()) != null) {
-                String data[] = line.split(":");
-                props.put(data[0].trim(), data[1].trim());
-            }
-            br.close();
-        } catch (FileNotFoundException ex) {
-            System.out.println("Archivo no encontrado, cerrando el programa");
-            System.exit(0);
-        } catch (IOException ex) {
-            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        return props;
     }
 
     private static Map<String, Object> readJsonFile(File f) {
