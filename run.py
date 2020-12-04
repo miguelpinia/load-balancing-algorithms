3f75e0a8863e2add2fc7edc0f72bd790c191743a
Miguel Angel Piña Avelino <miguel_pinia@ciencias.unam.mx>
2020-12-03 22:44:25 -0600

    Update experiments.

--- run.py
+++ run.py
@@ -7,19 +7,14 @@ alg_config_1 = ['CHASELEV', 'IDEMPOTENT_FIFO', 'IDEMPOTENT_LIFO',
 'WS_NC_MULT', 'B_WS_NC_MULT']
 alg_config_2 = ['CHASELEV', 'IDEMPOTENT_FIFO', 'IDEMPOTENT_LIFO',
 'WS_NC_MULT_LA', 'B_WS_NC_MULT_LA']
-alg_config_3 = ['CHASELEV', 'IDEMPOTENT_FIFO', 'IDEMPOTENT_LIFO',
-'NEW_B_WS_NC_MULT', 'NEW_B_WS_NC_MULT_LA']
-alg_config_4 = ['CHASELEV', 'WS_NC_MULT', 'B_WS_NC_MULT',
-'NEW_B_WS_NC_MULT', 'NEW_B_WS_NC_MULT_LA']
-alg_config_5 = ['CHASELEV', 'WS_NC_MULT_LA', 'B_WS_NC_MULT_LA',
-'NEW_B_WS_NC_MULT', 'NEW_B_WS_NC_MULT_LA']
-alg_config_6 = ['CHASELEV', 'WS_NC_MULT', 'B_WS_NC_MULT',
+alg_config_3 = ['CHASELEV', 'WS_NC_MULT', 'B_WS_NC_MULT',
 'WS_NC_MULT_LA', 'B_WS_NC_MULT_LA']
 
-configs = [alg_config_1, alg_config_2, alg_config_3, alg_config_4, alg_config_5, alg_config_6]
+configs = [alg_config_1, alg_config_2, alg_config_3]
 graphs = ['TORUS_2D', 'TORUS_2D_60', 'TORUS_3D', 'TORUS_3D_40', 'RANDOM']
 sizes = [1000000, 500000, 250000, 100000, 32768, 4096, 256]
 directeds = [True, False]
+allTime = [True, False]
 zeroCost = ['putSteals', 'putTakes', 'putsTakesSteals']
 ws = [(1,1), (3,3), (8,8)]
 
@@ -62,67 +57,73 @@ for config in configs:
                     print(j)
                     json.dump(j, outfile, indent=2)
                 os.system(scriptps)
-            else:
-                for (w, s) in ws:
-                    with open('config.json', 'w') as outfile:
-                        j = {'algorithms': config,
-                             cost: {'operations': 1000000,
-                                    'size': size,
-                                    'workers': w,
-                                    'stealers': s}}
-                        print(j)
-                        json.dump(j, outfile, indent=2)
-                    os.system(scriptpt)
+            # else:
+            #     for (w, s) in ws:
+            #         with open('config.json', 'w') as outfile:
+            #             j = {'algorithms': config,
+            #                  cost: {'operations': 1000000,
+            #                         'size': size,
+            #                         'workers': w,
+            #                         'stealers': s}}
+            #             print(j)
+            #             json.dump(j, outfile, indent=2)
+            #         os.system(scriptpt)
             print('Ending')
     for graph in graphs:
         if graph in ('TORUS_2D', 'TORUS_2D_60'):
             for size in sizes:
                 for directed in directeds:
-                    with open('config.json', 'w') as outfile:
-                        j = {'algorithms': config,
-                             'spanningTreeOptions' : {'graphType': graph,
-                                                      'vertexSize': 1000,
-                                                      'stepSpanningType': 'COUNTER',
-                                                      'iterations': 20,
-                                                      'directed': directed,
-                                                      'stealTime': False,
-                                                      'structSize': size}}
-                        print(j)
-                        json.dump(j, outfile, indent=2)
-                    os.system(script)
-                    print('Ending')
+                    for t in allTime:
+                        with open('config.json', 'w') as outfile:
+                            j = {'algorithms': config,
+                                 'spanningTreeOptions' : {'graphType': graph,
+                                                          'vertexSize': 1000,
+                                                          'stepSpanningType': 'COUNTER',
+                                                          'iterations': 20,
+                                                          'directed': directed,
+                                                          'stealTime': False,
+                                                          'structSize': size,
+                                                          'allTime': t}}
+                            print(j)
+                            json.dump(j, outfile, indent=2)
+                        os.system(script)
+                        print('Ending')
         elif graph in ('TORUS_3D', 'TORUS_3D_40'):
             for size in sizes:
                 for directed in directeds:
-                    with open('config.json', 'w')  as outfile:
-                        j = {'algorithms': config,
-                             'spanningTreeOptions': {'graphType': graph,
-                                                     'vertexSize': 100,
-                                                     'stepSpanningType': 'COUNTER',
-                                                     'iterations': 20,
-                                                     'directed': directed,
-                                                     'stealTime': False,
-                                                     'structSize': size}}
-                        print(j)
-                        json.dump(j, outfile, indent=2)
-                    os.system(script)
-                    print('Ending')
+                    for t in allTime:
+                        with open('config.json', 'w')  as outfile:
+                            j = {'algorithms': config,
+                                 'spanningTreeOptions': {'graphType': graph,
+                                                         'vertexSize': 100,
+                                                         'stepSpanningType': 'COUNTER',
+                                                         'iterations': 20,
+                                                         'directed': directed,
+                                                         'stealTime': False,
+                                                         'structSize': size,
+                                                         'allTime': t}}
+                            print(j)
+                            json.dump(j, outfile, indent=2)
+                        os.system(script)
+                        print('Ending')
         else:
             for size in sizes:
                 for directed in directeds:
-                    with open('config.json', 'w') as outfile:
-                        j = {'algorithms': config,
-                             'spanningTreeOptions': {'graphType': 'RANDOM',
-                                                     'vertexSize': 1000000,
-                                                     'stepSpanningType': 'COUNTER',
-                                                     'iterations': 20,
-                                                     'directed': directed,
-                                                     'stealTime': False,
-                                                     'structSize': size}}
-                        print(j)
-                        json.dump(j, outfile, indent=2)
-                    os.system(script)
-                    print('Ending')
+                    for t in allTime:
+                        with open('config.json', 'w') as outfile:
+                            j = {'algorithms': config,
+                                 'spanningTreeOptions': {'graphType': 'RANDOM',
+                                                         'vertexSize': 1000000,
+                                                         'stepSpanningType': 'COUNTER',
+                                                         'iterations': 20,
+                                                         'directed': directed,
+                                                         'stealTime': False,
+                                                         'structSize': size,
+                                                         'allTime': t}}
+                            print(j)
+                            json.dump(j, outfile, indent=2)
+                        os.system(script)
+                        print('Ending')
 # datetime object containing current date and time
 cwd = os.getcwd()
 now = datetime.now()
@@ -141,9 +142,18 @@ putsSteals = os.path.join(path, 'putsSteals')
 os.mkdir(putsSteals)
 putsTakesSteals = os.path.join(path, 'putsTakesSteals')
 os.mkdir(putsTakesSteals)
-# os.system('mv speedup*.png {}'.format(pspeedup))
-# os.system('mv time*.png {}'.format(timeup))
-# os.system('mv statistics*.png {}'.format(statistics))
+takesDir = os.path.join(path, 'takes')
+os.mkdir(takesDir)
+putsDir = os.path.join(path, 'puts')
+os.mkdir(putsDir)
+stealsDir = os.path.join(path, 'steals')
+os.mkdir(stealsDir)
+os.system('mv takes-*.png {}'.format(takesDir))
+os.system('mv puts-*.png {}'.format(putsDir))
+os.system('mv steals-*.png {}'.format(stealsDir))
+os.system('mv speedup*.png {}'.format(pspeedup))
+os.system('mv time*.png {}'.format(timeup))
+os.system('mv statistics*.png {}'.format(statistics))
 os.system('mv putsTakesSteals*.png {}'.format(putsTakesSteals))
 os.system('mv putsSteals*.png {}'.format(putsSteals))
 os.system('mv putsTakes*.png {}'.format(putsTakes))
