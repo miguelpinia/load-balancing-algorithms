b632584344dff243a0ebbdff4fba4520fb2c94b2
Miguel Angel Piña Avelino <miguel_pinia@ciencias.unam.mx>
2020-12-31 18:59:00 -0600

    Fix typo.

--- src/main/java/org/mx/unam/imate/concurrent/datastructures/graph/Vertex.java
+++ src/main/java/org/mx/unam/imate/concurrent/datastructures/graph/Vertex.java
@@ -43,7 +43,7 @@ public class Vertex {
 
     public void addNeighbour(int neighbour) {
         if (!neighbours.contains(neighbour)) {
-            neighbours.add(negihbour);
+            neighbours.add(neighbour);
         }
     }
 
