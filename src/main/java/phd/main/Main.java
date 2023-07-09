bf3767f8eabce6caa3807cf87814acf740152c2e
Miguel Angel Piña Avelino <miguel_pinia@ciencias.unam.mx>
2023-07-09 11:20:48 -0600

    Refactor Benchmarks for zero-cost and spanning-tree.

--- src/main/java/phd/main/Main.java
+++ src/main/java/phd/main/Main.java
@@ -5,35 +5,46 @@ import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.json.JSONObject;
-import phd.utils.WorkStealingUtils;
-import phd.ws.experiments.sat.concurrent.BruteSolver;
+import phd.ws.experiments.App;
 
 public class Main {
 
+    private static void callExperiments(JSONObject properties) {
+        App application = new App(properties);
+        application.executeExperiments();
+    }
+
     public static void main(String[] args) {
         //  TODO: Implement a version for choose any of experiments (MAPA 2023-06-12)
-//        String header
-//                = """
-//                  =====================================
-//                  =====================================
-//                  """;
-//        File f = new File("config.json");
-//        JSONObject props = readProperties(f);
-//        if (props == null) {
-//            System.out.println("Error: Can't read properties file. Try again.");
-//            return;
-//        }
-//        var footer = """
-//                     =================================
-//                     =        Properties read        =
-//                     =================================
-//                     """;
-//        System.out.println(header + props.toString(2) + footer);
+        // Current applications:
+        // - Zero cost experiments
+        // - Spanning tree
+        //   - Counting repeated work
+        //   - Statistical measurement
+        // - SAT
+        //   - Counting repeated work
+        //   - Statistical measurement
+
+        // First read config file and pass it as json structure to APP
+        if (args.length > 0) {
+            String option = args[0].strip();
+            JSONObject properties;
+            if ("--file".equals(option)) {
+                String fileName = args[1];
+                properties = readProperties(new File(fileName));
+                callExperiments(properties);
+            } else {
+                System.err.println("Error: Can't read properties file. Try again.");
+                System.exit(0);
+            }
+        } else {
+            JSONObject properties = readProperties(new File("config.json"));
+            callExperiments(properties);
+        }
+
 //        App battery = new App(props);
 //        battery.compareAlgs();
 //        String[][] formula = {{"-p", "q", "-r", "s"}, {"-q", "-r", "s"}, {"r"}, {"-p", "-s"}, {"-p", "r"}};
@@ -57,24 +68,35 @@ public class Main {
 //        } catch (InterruptedException ex) {
 //            Logger.getLogger(Solver.class.getName()).log(Level.SEVERE, null, ex);
 //        }
-
-        String configFile = "config_sat.json";
-        JSONObject properties = BruteSolver.readProperties(configFile);
-        int step = properties.getInt("step");
-//        JSONObject result = Statistics.evaluationSATST(configFile);
-        JSONObject result = BruteSolver.experimentMeasurements(configFile);
-        SimpleDateFormat format = new SimpleDateFormat("dd_MM_yyyy-HH:mm:ss");
-        String time = format.format(new Date());
-//        String title = String.format("experiment-brute-sat-st-%s-%d.json", time, structSize);
-        String title = String.format("experiment-brute-sat-measurements-%d-%s.json", step, time);
-        WorkStealingUtils.saveJsonObjectToFile(result, title);
-        System.out.println("Finish Evaluation");
+//        String configFile = "config_sat.json";
+//        JSONObject properties = BruteSolver.readProperties(configFile);
+//        int step = properties.getInt("step");
+////        JSONObject result = Statistics.evaluationSATST(configFile);
+//        JSONObject result = BruteSolver.experimentMeasurements(configFile);
+//        SimpleDateFormat format = new SimpleDateFormat("dd_MM_yyyy-HH:mm:ss");
+//        String time = format.format(new Date());
+////        String title = String.format("experiment-brute-sat-st-%s-%d.json", time, structSize);
+//        String title = String.format("experiment-brute-sat-measurements-%d-%s.json", step, time);
+//        WorkStealingUtils.saveJsonObjectToFile(result, title);
+//        System.out.println("Finish Evaluation");
     }
 
     private static JSONObject readProperties(File f) {
         try {
+            var header
+                    = """
+                  =====================================
+                  =====================================
+                  """;
+            var footer = """
+                     =================================
+                     =        Properties read        =
+                     =================================
+                     """;
+
             String json = new String(Files.readAllBytes(Paths.get(f.getName())), StandardCharsets.UTF_8.displayName());
             JSONObject obj = new JSONObject(json);
+            System.out.println(header + obj.toString(2) + footer);
             return obj;
         } catch (IOException ex) {
             Logger.getLogger(Main.class.getName()).log(Level.SEVERE, "ERROR: Can't read properties file", ex);
---
+++ src/main/java/phd/ws/experiments/zero/BenchmarkZeroCost.java
@@ -0,0 +1,116 @@
+package phd.ws.experiments.zero;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+import org.json.JSONObject;
+import phd.main.Constants;
+import phd.utils.WorkStealingUtils;
+import phd.ws.AlgorithmsType;
+import phd.ws.experiments.Experiments;
+
+/**
+ *
+ * @author miguel
+ */
+public class BenchmarkZeroCost {
+
+    private JSONObject processCoVPutSteals(Map<String, CoVResultZero> covs) {
+        JSONObject data = new JSONObject();
+        covs.forEach((algName, cov) -> {
+            JSONObject results = new JSONObject();
+            results.put("totalMean", cov.getMean());
+            results.put("putsMean", cov.getMeanPuts());
+            results.put("stealsMean", cov.getMeanSteals());
+            data.put(algName, results);
+        });
+        return data;
+    }
+
+    private JSONObject processCoVPutTakes(Map<String, CoVResultZero> covs) {
+        JSONObject data = new JSONObject();
+        covs.forEach((algName, cov) -> {
+            JSONObject results = new JSONObject();
+            results.put("totalMean", cov.getMean());
+            results.put("putsMean", cov.getMeanPuts());
+            results.put("takesMean", cov.getMeanTakes());
+            data.put(algName, results);
+        });
+        return data;
+    }
+
+    private void generateJSONFile(JSONObject results, String fileName,
+            int structSize, int numOperations) {
+        System.out.println(results.toString(2));
+        SimpleDateFormat format = new SimpleDateFormat("dd_MM_yyyy-HH:mm:ss");
+        String time = format.format(new Date());
+        String title = String.format("%s-%d-%d-%s.json", fileName, structSize, numOperations, time);
+        WorkStealingUtils.saveJsonObjectToFile(results, title);
+        System.out.println(String.format("Writing to file: %s", title));
+    }
+
+    public void benchmarkPutSteals(JSONObject putStealsOptions, List<AlgorithmsType> types) {
+        var header = """
+                        =====================================
+                        = generating experiment puts-steals =
+                        =====================================
+                        """;
+        var footer = """
+                     =====================================
+                     = Finish benchmark for puts-steals  =
+                     =====================================
+                     """;
+        System.out.println(header);
+        int structSize = putStealsOptions.getInt(Constants.STRUCT_SIZE);
+        int numOperations = putStealsOptions.getInt(Constants.NUM_OPERATIONS);
+        System.out.println(String.format("The benchmark will be performed with a size %d and a total of %d operations", structSize, numOperations));
+        Map<String, CoVResultZero> covs = ZeroCost.fullExperimentPutsSteals(types, putStealsOptions);
+        JSONObject results = processCoVPutSteals(covs);
+        generateJSONFile(results, "stats-puts-steals", structSize, numOperations);
+        System.out.println(footer);
+
+    }
+
+    public void benchmarkPutTakes(JSONObject putTakesOptions, List<AlgorithmsType> types) {
+        var header = """
+                      =====================================
+                      = generating experiment puts-takes  =
+                      =====================================
+                      """;
+        var footer = """
+                     =====================================
+                     = Finish benchmark for puts-takes   =
+                     =====================================
+                     """;
+        System.out.println(header);
+        int structSize = putTakesOptions.getInt(Constants.STRUCT_SIZE);
+        int numOperations = putTakesOptions.getInt(Constants.NUM_OPERATIONS);
+        System.out.println(String.format("The benchmark will be performed with a size %d and a total of %d operations", structSize, numOperations));
+        Map<String, CoVResultZero> covs = ZeroCost.fullExperimentPutsTakes(types, putTakesOptions);
+        JSONObject results = processCoVPutTakes(covs);
+        generateJSONFile(results, "stats-puts-takes", structSize, numOperations);
+        System.out.println(footer);
+    }
+
+    public void benchmarkPutTakesSteals(JSONObject ptsOptions, List<AlgorithmsType> types) {
+        String header = """
+                      ============================================
+                      = generating experiment puts-takes-steals  =
+                      ============================================
+                      """;
+        var footer = """
+                     ============================================
+                     = Finish benchmark for puts-takes-steals   =
+                     ============================================
+                     """;
+        System.out.println(header);
+        int structSize = ptsOptions.getInt(Constants.STRUCT_SIZE);
+        int numOperations = ptsOptions.getInt(Constants.NUM_OPERATIONS);
+        System.out.println(String.format("The benchmark will be performed with a size %d and a total of %d operations", structSize, numOperations));
+        Experiments exp = new Experiments();
+        JSONObject results = exp.putTakesSteals(types, ptsOptions);
+        generateJSONFile(results, "puts-takes-steals", structSize, numOperations);
+    }
+
+}
